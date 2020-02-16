1753f8407f0ed67d6113d57b9cd7c0b74de0dde3
orignal <badardani11@gmail.com>
2020-02-16 14:49:15 +0000

    level MD5

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
-require("dotenv").config();
+//require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 =  require("md5");
 const app = express();
 mongoose.connect('mongodb://localhost:27017/secretDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -19,8 +19,6 @@ const userSchema = new mongoose.Schema({                //loginSchema
     email : String,
     password : String
 });
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
 
 const User = mongoose.model("User",userSchema);
 
@@ -44,7 +42,7 @@ app.route("/register")
     .post(function (req, res) {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password);
         });
         console.log(newUser.email);
         newUser.save(function (err) {
@@ -62,7 +60,7 @@ app.post("/login", function (req, res) {
 
 
      const  email= req.body.username;
-     const  password= req.body.password;
+     const  password= md5(req.body.password);
 
      User.findOne({email:email},function (err, found) {
       if(err){
